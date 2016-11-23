51d98bd3f7dc8020ab7c9160ac1dafbaa966b409
deniseho <deniseho630@gmail.com>
2016-11-23 13:50:28 +0800

    add EnsureLoggedInContainer

---
+++ src/components/common/EnsureLoggedInContainer.js
@@ -0,0 +1,16 @@
+import React, {PropType} from 'react';
+import {connect} from 'react-redux';
+
+class EnsureLoggedInContainer extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+      return(
+      <h1>EnsureLoggedInContainer</h1>
+      )
+  }
+}
+
+export default EnsureLoggedInContainer;
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,21 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import Login from './components/login/Login';
+import EnsureLoggedInContainer from './components/common/EnsureLoggedInContainer.js';
 import CoursesPage from './components/course/CoursesPage';
 import ManageCoursePage from './components/course/ManageCoursePage'; //eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
-    <Route path="courses" component={CoursesPage} />
-    <Route path="course" component={ManageCoursePage} />
-    <Route path="course/:id" component={ManageCoursePage} />
-    <Route path="about" component={AboutPage} />
+    <Route path="login" component={Login}/>
+
+    <Route component={EnsureLoggedInContainer}>
+      <IndexRoute component={HomePage} />
+      <Route path="courses" component={CoursesPage} />
+      <Route path="course" component={ManageCoursePage} />
+      <Route path="course/:id" component={ManageCoursePage} />
+      <Route path="about" component={AboutPage} />
+    </Route>
   </Route>
 );
