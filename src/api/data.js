b167ed353a7b23d7dcaa96a9c91d7dc204c8a3f7
deniseho <deniseho630@gmail.com>
2016-11-23 17:36:06 +0800

    add mock data

--- src/api/data.js
+++ src/api/data.js
@@ -1,44 +1,9 @@
-// const data =
-// [
-//     { CPC: 5, CTR: 3, filter1: 2, filter2: 7 },
-//     { CPC: 10, CTR: 17, filter1: 1, filter2: 3 },
-//     { CPC: 15, CTR: 4, filter1: 4, filter2: 6 },
-//     { CPC: 12, CTR: 6, filter1: 6, filter2: 8 },
-//     { CPC: 2, CTR: 8, filter1: 9, filter2: 11 }
-// ];
+const data =[
+    { CPC: 5, CTR: 3 },
+    { CPC: 10, CTR: 17 },
+    { CPC: 15, CTR: 4 },
+    { CPC: 12, CTR: 6 },
+    { CPC: 2, CTR: 8 }
+];
 
-const monthlySales = [
-        {
-            "month": 1,
-            "sales": 100
-        }, {
-            "month": 2,
-            "sales": 130
-        }, {
-            "month": 3,
-            "sales": 250
-        }, {
-            "month": 4,
-            "sales": 300
-        }, {
-            "month": 5,
-            "sales": 265
-        }, {
-            "month": 6,
-            "sales": 225
-        }, {
-            "month": 7,
-            "sales": 180
-        }, {
-            "month": 8,
-            "sales": 120
-        }, {
-            "month": 9,
-            "sales": 145
-        }, {
-            "month": 10,
-            "sales": 130
-        }
-    ];
-
-export default monthlySales;
+export default data;
