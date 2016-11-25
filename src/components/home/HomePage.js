5a764f0872dc35d4e178b2f3acc46269028b43bf
deniseho <deniseho630@gmail.com>
2016-11-25 17:17:38 +0800

    modify homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,23 +5,14 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as dataActions from '../../actions/dataActions.js';
 
-import DotChart from '../chart/DotChart.js';
-import Uploader from '../common/Uploader.js';
-import SelectInput from '../common/SelectInput';
-
 class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
   }
 
   render() {
-    const {data} = this.props;
-
     return (
-      <div>
-        <Uploader/>
-        <DotChart data={data}/>
-      </div>
+      <h1>home</h1>
     );
   }
 }
