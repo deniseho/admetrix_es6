39b509adcfd564b6dcb07ace50c5c98b1e647b9d
deniseho <deniseho630@gmail.com>
2016-12-09 18:27:01 +0800

    modify api to fetch data from json-server

--- src/api/mockDataApi.js
+++ src/api/mockDataApi.js
@@ -1,72 +1,6 @@
-import delay from './delay';
-
-const entireData = [
-    {
-        adId: 'adId-01',
-        adName: 'adName-01',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: true,
-        CPC: 15,
-        CTR: 23
-    }, {
-        adId: 'adId-02',
-        adName: 'adName-02',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 4,
-        CTR: 7
-    }, {
-        adId: 'adId-03',
-        adName: 'adName-03',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 1,
-        CTR: 3
-    }, {
-        adId: 'adId-04',
-        adName: 'adName-04',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 22,
-        CTR: 6
-    }, {
-        adId: 'adId-05',
-        adName: 'adName-05',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 14,
-        CTR: 5
-    }, {
-        adId: 'adId-06',
-        adName: 'adName-06',
-        projId: "projId-01",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 8,
-        CTR: 2
-    }, {
-        adId: 'adId-07',
-        adName: 'adName-07',
-        projId: "projId-02",
-        adSetId: 'adSetId-02',
-        isUser: false,
-        CPC: 9,
-        CTR: 11
-    }, {
-        adId: 'adId-08',
-        adName: 'adName-08',
-        projId: "projId-02",
-        adSetId: 'adSetId-03',
-        isUser: false,
-        CPC: 21,
-        CTR: 22
-    }
-]
+import delay from './delay'
+import fetch from 'isomorphic-fetch'
+require('es6-promise').polyfill();
 
 const selfData = {
     axisFilters: [
@@ -134,39 +68,50 @@ const selfData = {
 class DataApi {
     static getEntireData() {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(Object.assign([], entireData));
-            }, delay);
+            fetch('http://localhost:3000/entireData').then((response) => {
+                response
+                    .json()
+                    .then((data) => {
+                        resolve(Object.assign([], data));
+                    }, delay);
+            })
+
         });
     }
 
     static getAxisFilters() {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(Object.assign([], selfData.axisFilters));
-            }, delay);
+            fetch('http://localhost:3000/selfData').then((response) => {
+                response
+                    .json()
+                    .then((data) => {
+                        resolve(Object.assign([], data.axisFilters));
+                    })
+            })
         });
     }
 
     static getDataFilters() {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(Object.assign({}, {
-                    projects: selfData.projects,
-                    adSets: selfData.adSets,
-                    ads: selfData.ads,
-                    categories: selfData.categories
-                }));
-            }, delay);
+            fetch('http://localhost:3000/selfData').then((response) => {
+                response
+                    .json()
+                    .then((data) => {
+                        resolve(Object.assign({}, {
+                            projects: data.projects,
+                            adSets: data.adSets,
+                            ads: data.ads,
+                            categories: data.categories
+                        }))
+                    })
+            })
         });
     }
 
-    static selectOptions(selected){
-        console.log("selected: " + JSON.stringify(selected))
+    static selectOptions(selected) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve( 
-                    Object.assign({}, {
+                resolve(Object.assign({}, {
                     // project: "projId-02",
                     project: "",
                     adSet: "",
